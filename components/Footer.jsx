59ac5e48cf5db47f52aaf67645ff80282aeafbbe
beejsbj <burooj.bj@gmail.com>
2023-10-16 14:17:47 -0400

    Move link to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,12 @@ export default function Footer() {
   return (
     <footer>
       <inner-column>
-        <a href="https://rainbow.me" rel="noopener noreferrer" target="_blank">
+        <a
+          className="text"
+          href="https://rainbow.me"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
           API3 demo
         </a>
       </inner-column>
