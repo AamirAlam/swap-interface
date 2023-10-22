68cbc4dbc6716fc1a00fd1874fe54f7e4ec5a027
beejsbj <burooj.bj@gmail.com>
2023-10-22 16:03:06 -0400

    Commit new meta info

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,22 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Swap</title>
-        <meta content="API3 swap demo" name="description" />
+		  <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href="/images/swap.svg" rel="icon" />
+
+		  <meta property="og:image" content="/images/swap-meta.jpg" />
+
+        <meta content="API3's Demo of its Oracle for Swapping and Liquidity" name="description" />
+
+		  <meta property="og:locale" content="en_US" />
+		  
+
+		  {/* theme color */}
+		  <meta name="theme-color" content="#a785e2" />
+
+
+
+
       </Head>
 
       <section>
